83a60c55d41ff1a147be9c4064cbef0f6f2e48d7
miz <mizugokoro@gmail.com>
2016-01-31 15:32:18 +0000

    added company model

---
+++ db/migrate/20160131153156_create_companies.rb
@@ -0,0 +1,22 @@
+class CreateCompanies < ActiveRecord::Migration
+  def change
+    create_table :companies do |t|
+      t.string :ticker
+      t.string :name
+      t.string :year
+      t.decimal :fixed_asset
+      t.decimal :current_asset
+      t.decimal :long-term_liabilities
+      t.decimal :short-term_liabilities
+      t.decimal :revenue
+      t.decimal :operating_income
+      t.decimal :ibit
+      t.decimal :net_income
+      t.decimal :operation_cashflow
+      t.decimal :financing_cashflow
+      t.decimal :investment_cashflow
+
+      t.timestamps
+    end
+  end
+end
